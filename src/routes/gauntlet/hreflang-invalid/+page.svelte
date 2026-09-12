17f8b936e01e9a319c42aa4d7eef25d85fc07d8f
Tito2912 <caron.tony78@outlook.fr>
2026-09-12 17:33:52 +0200

    fix(seo): duplicate_titles — src/routes/gauntlet/hreflang-invalid/+page.svelte
    Generated by SEO Agent

--- src/routes/gauntlet/hreflang-invalid/+page.svelte
+++ src/routes/gauntlet/hreflang-invalid/+page.svelte
@@ -5,7 +5,7 @@
 
 <svelte:head>
   <meta name="viewport" content="width=device-width" />
-  <title>Page de test du parcours d'obstacles Noyaru</title>
+  <title>Parcours d'obstacles Noyaru - Double Slash</title>
   <meta name="description" content="Découvrez le parcours d'obstacles Noyaru, une expérience unique et stimulante, conçue pour tester vos compétences et votre agilité." />
   <link rel="canonical" href="https://noyaru-stack-sveltekit.netlify.app/gauntlet/hreflang-invalid" />
   <link rel="alternate" hreflang="fr_FR" href="https://noyaru-stack-sveltekit.netlify.app/gauntlet/hreflang-invalid" />
